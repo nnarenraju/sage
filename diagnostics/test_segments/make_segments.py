503cfc09c5bd5cd7d8f1a7679c7328f40820c082
nnarenraju <nnarenraju@gmail.com>
2021-12-17 12:53:36 +0530

    Simplified make_segments.py and added annotations

--- diagnostics/test_segments/make_segments.py
+++ diagnostics/test_segments/make_segments.py
@@ -24,49 +24,52 @@ Documentation: NULL
 """
 
 # IN-BUILT
+import os
 import numpy as np
+    
+def make_segments(GPS_start_time: int,
+                  gap: float,
+                  segment_length: float,
+                  ninjections: int,
+                  output_filepath: str,
+                  force: bool) -> None:
 
-class Segments:
     """
+    Description
+    -----------
+    Make segments CSV file used by ligo.segments
+    Necessary fields = (id, GPS_start_time, GPS_end_time)
+    
+    Parameters
+    ----------
+    
+    
+    
+    Returns
+    -------
+    None.
     
     """
+    duration = segment_length*ninjections
+    # Consider nsegments as lower limit
+    offset = duration%segment_length
+    start_time = GPS_start_time
+    end_time = GPS_start_time + duration + offset
+    nsegments = int((duration+offset)//segment_length)
+    # Last point will be end time (ignore)
+    start_times = np.linspace(start_time, end_time, nsegments, endpoint=False)
+    gap = np.full(len(start_times[1:]), gap)*np.arange(1, len(start_times[1:])+1)
+    start_times = np.concatenate(([start_times[0]], start_times[1:] + gap))
+    start_times = start_times.astype(np.int16)
+    # End-points
+    end_times = (start_times + segment_length).astype(np.int16)
+    segments = np.column_stack((range(len(start_times)), start_times, end_times))
+    # Savedata (segment.csv)
+    if force:
+        try:
+            print(f"--force: {output_filepath} exists. Overwriting.")
+            os.remove(output_filepath)
+        except OSError:
+            pass
     
-    def __init__(self):
-        # Times
-        self.GPS_start_time = 0.0
-        self.duration = 2.0e+5
-        # No correlation b/w segments
-        self.gap = 1.0
-        # Segment details
-        # equal_segments: duration is a soft limit, segment_length is strict
-        # unequal_segments: duration is hard limit, segment_length is soft
-        self.equal_segments = True
-        # For equal_segments==True
-        self.segment_length = 20.0
-        # For equal_segments==False
-        self.unequal_segments_llimit = 10.0
-        self.unequal_segments_ulimit = 30.0
-        # Output
-        self.output_filepath = "./segments.csv"
-    
-    def make_segments(self):
-        # Make segments CSV file used by ligo.segments
-        # Necessary fields = (id, GPS_start_time, GPS_end_time)
-        if self.equal_segments:
-            # Consider nsegments as lower limit
-            offset = self.duration%self.segment_length
-            start_time = self.GPS_start_time
-            end_time = self.GPS_start_time + self.duration + offset
-            nsegments = int((self.duration+offset)//self.segment_length)
-            # Last point will be end time (ignore)
-            start_times = np.linspace(start_time, end_time, nsegments, endpoint=False)
-            gap = np.full(len(start_times[1:]), self.gap)*np.arange(1, len(start_times[1:])+1)
-            start_times = np.concatenate(([start_times[0]], start_times[1:] + gap))
-            start_times = start_times.astype(np.int16)
-            # End-points
-            end_times = (start_times + self.segment_length).astype(np.int16)
-            segments = np.column_stack((range(len(start_times)), start_times, end_times))
-            #print(max(segments.flatten()))
-            np.savetxt("segments.csv", segments, delimiter=",", fmt="%d")
-        else:
-            raise ValueError("make_segments: unequal lengths under construction!")
+    np.savetxt(output_filepath, segments, delimiter=",", fmt="%d")
