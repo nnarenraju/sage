3d2675a5daffd59316d988c7b74f96cabaffd41a
nnarenraju <nnarenraju@gmail.com>
2021-12-17 12:52:49 +0530

    Wrote a function to call make_segments and generate_data script

--- diagnostics/test_segments/make_dataset.py
+++ diagnostics/test_segments/make_dataset.py
@@ -31,8 +31,8 @@ From Pseudo-code:
     7. Create a large HDF5 file containing the details (ID, path, target)
 
 Steps:
-    1. Call make_segments.py using appropriate opts to create segments.csv
-    2. Call generate_data.py from ML-MDC1 using segments.csv to make training data
+    1. Call make_segments.py using appropriate opts to create segments.csv (FIN)
+    2. Call generate_data.py from ML-MDC1 using segments.csv to make training data (FIN)
     3. Verify whether all params and segments are as intended
     4. Cross verify the prior distribution with the observed distribution
     5. Cross verify the location of the GW signal and its 'tc'
@@ -47,30 +47,121 @@ Steps:
 import os
 import sys
 import h5py
-import numpy as np
+import uuid
+import warnings
+import datetime
 
 # LOCAL
-from make_segments import Segments
+from make_segments import make_segments as mksegments
 # Add MLMDC1 repo from GWastro
 # insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, '../../external/mlmdc1')
+sys.path.insert(1, '../../external/ml-mock-data-challenge-1')
 from generate_data import main as gendata
 
-def call_gendata():
-    raw_args = ['--data-set', str(3)]
-    raw_args += ['--output-injection-file', "injections.hdf"]
-    raw_args += ['--output-foreground-file', "foreground.hdf"]
-    raw_args += ['--output-background-file', "background.hdf"]
-    raw_args += ['--seed', str(42)]
-    raw_args += ['--start-offset', str(0)]
-    raw_args += ['--duration', str(2000)]
-    raw_args += ['--verbose']
-    raw_args += ['--force']
+
+class GenerateData:
+    """
     
-    gendata(raw_args)
-
-
-if __name__ == "__main__":
+    """
     
-    call_gendata()
+    def __init__(self):
+        ## generate_data script parameters
+        # Type of dataset (1, 2, 3 or 4)
+        # Refer https://github.com/gwastro/ml-mock-data-challenge-1/wiki/Data-Sets 
+        self.dataset = 3
+        # Path to store output HDF5 files
+        self.output_injection_file = "injections.hdf"
+        self.output_foreground_file = "foreground.hdf"
+        self.output_background_file = "background.hdf"
+        # Random seed provided to generate_data script
+        # This will be unique and secret for the testing set
+        self.seed = 42
+        # Dataset params
+        self.start_offset = 0
+        self.dataset_duration = 2000
+        # Other options
+        self.verbose = True
+        self.force = False
+        
+        ## make_injections using pycbc_create_injections (uses self.seed)
+        # params will be used to call above function via generate_data.py
+        # pycbc_create_injections has been modified by nnarenraju (Dec 15th, 2021)
+        self.output_segment_file = ""
+        # Start time of segments
+        self.segment_GPS_start_time = 0.0
+        # Distance b/w two adjacent 'tc'
+        # For training set, set to a value larger than segment length
+        self.time_step = 20.0
+        # Time window within which to place the merger
+        # 'tc' is located within this window
+        self.time_window_llimit = None
+        self.time_window_ulimit = None
+        # Length of segment/duration (in seconds)
+        self.segment_length = 20.0
+        self.ninjections = 0
+        # Gap b/w adjacent segments (if any)
+        self.segment_gap = None
+        
+        # Metadata and identification
+        self.id = uuid.uuid4().hex
+        self.creation_time = datetime.datetime.now()
+    
+    def __str__(self):
+        pass
+    
+    def make_segments(self):
+        # Make segments.csv to be used by generate_data script
+        # Create an object for Segments class
+        args =  (self.segment_GPS_start_time,)
+        args += (self.segment_gap,)
+        args += (self.segment_length,)
+        args += (self.ninjections,)
+        args += (self.output_segment_file,)
+        args += (self.force)
+        
+        # Make segments.csv (equal length assumed)
+        mksegments(*args)
 
+    def call_gendata(self):
+        # Main params to call generate_data script
+        # Warning! for dataset 4 large noise data download
+        if self.dataset == 4:
+            warnings.warn("Dataset type 4 downloads an ~94GB noise file.")
+            
+        raw_args =  ['--data-set', str(self.dataset)]
+        raw_args += ['--output-injection-file', self.output_injection_file]
+        raw_args += ['--output-foreground-file', self.output_foreground_file]
+        raw_args += ['--output-background-file', self.output_background_file]
+        
+        # sanity check for segments.csv
+        if os.path.exists(self.output_segment_file):
+            raw_args += ['--input-segment-file', self.output_segment_file]
+        else:
+            raise IOError("{} does not exist!".format(self.output_segment_file))
+            
+        raw_args += ['--seed', str(self.seed)]
+        raw_args += ['--start-offset', str(self.start_offset)]
+        raw_args += ['--duration', str(self.dataset_duration)]
+        
+        # MISC params
+        if self.verbose:
+            raw_args += ['--verbose']
+        if self.force:
+            raw_args += ['--force']
+        
+        gendata(raw_args)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
