993853660a2e463e861dd99f18c729a5362abac1
nnarenraju <nnarenraju@gmail.com>
2022-01-23 19:46:46 +0000

    Fixed a bug where start time was not converted to int before usage

--- diagnostics/test_dataset/verify_signals.py
+++ diagnostics/test_dataset/verify_signals.py
@@ -66,7 +66,7 @@ def _get_data(path):
         # Common time axis for all data
         start_time = times_1[0]
         sample_rate = 1.0/attrs['delta_t']
-        end_time = start_time + (len(data_1)//sample_rate)
+        end_time = int(start_time) + (len(data_1)//sample_rate)
         time_axis = np.linspace(start_time, end_time, len(data_1), dtype=np.int32)
         return (data_1, data_2, time_axis, dets)
 
