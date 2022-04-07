65eb816fc79ae8cdf9712f68fcf1aa0344183277
Narenraju Nagarajan <nnarenraju@gmail.com>
2022-04-07 17:00:40 +0100

    Added new config for Kaggle first place model

--- source/baseline/configs.py
+++ source/baseline/configs.py
@@ -34,7 +34,7 @@ from sklearn.model_selection import StratifiedKFold
 from data.datasets import MLMDC1
 from metrics.custom_metrics import AUC
 from architectures.backend import CNN_1D
-from architectures.frontend import AlphaModel, BetaModel, GammaModel
+from architectures.frontend import AlphaModel, BetaModel, GammaModel, KappaModel
 from data.transforms import Unify, Normalise, BandPass, Whiten, MultirateSampling
 from losses.custom_loss_functions import BCEgw_MSEtc, regularised_BCELoss
 
@@ -68,13 +68,19 @@ class Baseline:
     
     model_params = dict(
         # Simple NN Model
-        model_name='mlmdc_example',
-        pretrained=False,
+        model_name = 'mlmdc_example',
         in_channels = 2,
         out_channels = 2,
         store_device = 'cpu',
-        weights_path = 'weights.pt'
     )
+    
+    # Pre-trained weights (does not refer to pretrained timm models)
+    # This refers to a pretrained frontend+backend model on GW data
+    pretrained = False
+    # Provide a weights path even if pretrained==False
+    # This location will be used to store the best weights
+    # Just the file_name is enough, automagically stored in export_dir
+    weights_path = 'weights.pt'
     
     """ Save trainable train and valid data """
     # Saves trainable data after transforms are applied as HDF5 files
@@ -144,136 +150,91 @@ class Baseline:
     verbose = False
     
     
-""" MANUAL BASELINE """
-
-class ManualBaseline:
-    
-    """ Data storage """
-    name = "ManualBaseline"
-    # Directory to store output from pipeline
-    export_dir = Path("/home/nnarenraju")
-    
-    """ Dataset Splitting """
-    # Folds are made by preserving the percentage of samples for each class
-    splitter = None
-    
-    """ Dataset """
-    # Dataset object (opts, quick access, read only)
-    dataset = MLMDC1
-    dataset_params = dict()
-    
-    """ Architecture """
-    model = None
-    
-    model_params = dict(
-        # Simple NN Model
-        model_name='mlmdc_manual_example',
-        pretrained=False,
-        in_channels = 2,
-        out_channels = 1
-    )
-    
-    """ Epochs and Batches """
-    num_steps = 25000
-    num_epochs = 50
-    batch_size = 4
-    
-    """ Loss Function """
-    # Add the () as suffix to loss function, returns object instead
-    loss_function = None
-    
-    """ Data Transforms """
-    # Input to Unfy should always be a list
-    # Input to transforms should be a dict
-    transforms = dict(
-        train=Unify([
-            Normalise(factors=[1.8021542328645444e-19, 9.216145461527009e-20]),
-        ]),
-        test=Unify([
-            Normalise(factors=[1.8021542328645444e-19, 9.216145461527009e-20]),
-        ]),
-        target=None
-    )
-    
-    # Debugging (size: train_data = 1e4, val_data = 1e3)
-    debug = False
-    
-    
-    
     
 """ CUSTOM MODELS FOR EXPERIMENTATION """
 
-class LonelyTimm:
+class KaggleFirst:
     
-    """ Name and Path """
-    name = "LonelyTimm"
-    export_dir = ""
+    """ Data storage """
+    name = "Baseline_kaggle_test"
+    export_dir = Path("/Users/nnarenraju/Desktop") / name
     
     """ Dataset Splitting """
-    # Number of folds (must be at least 2, default = 5)
-    n_splits = 5
-    # Seed for K-Fold shuffling
-    seed = 150914
-    # Folds are made by preserving the percentage of samples for each class
-    splitter = StratifiedKFold(n_splits, shuffle=True, random_state=seed)
+    n_splits = 2
+    seed = 42
+    splitter = None
     
     """ Dataset """
-    # Dataset object (opts, quick access, read only)
     dataset = MLMDC1
     dataset_params = dict()
     
     """ Architecture """
-    model = BetaModel
+    model = KappaModel
     
     model_params = dict(
-        # Timm Model
-        model_name='resnet34',
-        timm_params={'in_chans':3},
-        pretrained=False,
-        num_classes=1,
-        # Trainable Backend
-        backend=CNN_1D,
-        backend_params=dict(
-            in_channels = 3,
-            out_channels = 3,
-            force_out_size = 128,
-            check_out_size = True
-        )
+        # Kaggle frontend+backend
+        # This model is ridiculously slow on cpu, use cuda:0
+        model_name = 'kaggle_first', 
+        filter_size = 16,
+        kernel_size = 32,
+        timm_params = {'model_name': 'resnet34', 
+                       'pretrained': True, 
+                       'in_chans': 2, 
+                       'drop_rate': 0.25},
+        store_device = 'cuda:0',
     )
+    
+    pretrained = False
+    weights_path = 'weights.pt'
+    
+    """ Save trainable train and valid data """
+    save_trainable_dataset = False
     
     """ Epochs and Batches """
     num_steps = 25000
-    num_epochs = 15
-    batch_size = 8
+    num_epochs = 25
+    batch_size = 100
+    save_freq = 5
+    
+    """ Gradient Clipping """
+    clip_norm = 100
     
     """ Optimizer """
     optimizer = optim.Adam
-    optimizer_params = dict(lr=1e-4, weight_decay=1e-6)
+    optimizer_params = dict(lr=5e-5, weight_decay=1e-6)
     
     """ Scheduler """
-    scheduler = CosineAnnealingWarmRestarts
-    scheduler_params = dict(T_0=5, T_mult=1, eta_min=1e-6)
+    scheduler = None
+    scheduler_params = dict()
     scheduler_target = None
     batch_scheduler = False
     
     """ Loss Function """
-    loss_function = nn.BCEWithLogitsLoss()
+    loss_function = regularised_BCELoss(dim=2)
+    output_loss_file = "losses.txt"
     
     """ Evaluation Metric """
-    eval_metric = AUC().torch
+    eval_metric = None
+    accuracy_thresh = 0.5
+    
+    """ Storage Devices """
+    store_device = 'cuda:0'
+    train_device = 'cuda:0'
     
     """ Data Transforms """
     transforms = dict(
         train=Unify([
-            Normalise(factors=[1.0, 1.0]),
-            BandPass(lower=12, upper=512),
+            BandPass(lower=16, upper=512, fs=2048., order=6),
+            Whiten(trunc_method='hann', remove_corrupted=True),
+            MultirateSampling(),
         ]),
         test=Unify([
-            Normalise(factors=[1.0, 1.0]),
-            BandPass(lower=12, upper=512),
+            BandPass(lower=16, upper=512, fs=2048., order=6),
+            Whiten(trunc_method='hann', remove_corrupted=True),
+            MultirateSampling(),
         ]),
         target=None
     )
     
-    # Debugging (size: train_data = 1e4, val_data = 1e3)
     debug = False
+    verbose = False
