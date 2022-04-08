10592c41d367420c10df50efc0d5e30f9e119ce6
Narenraju Nagarajan <nnarenraju@gmail.com>
2022-04-08 19:03:19 +0100

    Changed the lookup table search location to trainable_batched_dataset dir

--- source/baseline/data/prepare_data.py
+++ source/baseline/data/prepare_data.py
@@ -164,8 +164,7 @@ class DataModule:
         
         # set lookup table
         if cfg.dataset.__name__ == "Simple":
-            lookup_table = os.path.join(cfg.export_dir, "trainable_batched_dataset")
-            lookup_table = os.path.join(lookup_table, "trainable.json")
+            lookup_table = os.path.join(cfg.export_dir, "trainable.json")
             with open(lookup_table, 'r') as fp:
                 # train should have (ids, path, target)
                 train = json.load(fp)
