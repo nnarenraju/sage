f1b60bf8dd22e277728646dee82d97b85ea30ed5
Narenraju Nagarajan <nnarenraju@gmail.com>
2022-07-13 09:27:20 +0100

    Fixing bugs in the PE methods

--- source/baseline/manual.py
+++ source/baseline/manual.py
@@ -501,6 +501,8 @@ def train(cfg, data_cfg, Network, optimizer, scheduler, loss_function, trainDL, 
                     for key, value in validation_labels.items():
                         validation_labels[key] = value.to(dtype=torch.float32, device=cfg.train_device)
                     
+                    print(validation_labels['gw'].device)
+                    
                     batch_validation_loss = 0.
                     accuracies = []
                     pred_prob = []
