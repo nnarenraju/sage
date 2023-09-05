508017fbcc9f7081b7ca8a2fc45513e8b639726e
Narenraju Nagarajan <nnarenraju@gmail.com>
2023-09-05 15:56:18 +0100

    Minor changes to data_cfg for experimentation

--- source/baseline/data_configs.py
+++ source/baseline/data_configs.py
@@ -49,8 +49,8 @@ class Default:
     # Data storage drive or /mnt absolute path
     parent_dir = "/local/scratch/igr/nnarenraju"
     # Dataset directory within parent_dir
-    data_dir = "dataset_1e6_Jan27_D4_seed42"
-    # data_dir = "buffer_datadir"
+    # data_dir = "buffer_dataset"
+    data_dir = "dataset_D4_2e6_Aug23_seed42_shortSamples"
     
     """ Basic dataset options """
     # These options are used by generate_data.py
@@ -65,13 +65,16 @@ class Default:
     save_injection_priors = True
     
     """ Number of samples """
-    # For now, keep both values equal
-    num_waveforms = 500000
-    num_noises = 500000
+    # Keep both values equal (balanced dataset)
+    # For imbalanced dataset, change the class weights in loss function
+    # instead of changing the data generation procedures.
+    num_waveforms = 1000
+    num_noises = 1000
     # For efficient RAM usage in data generation
-    # Here too, keep both nums equal
+    # Here too, keep both nums equal (Each chunk will be class balanced)
     # chunk_size = [num_waveforms_chunk, num_noises_chunk]
-    chunk_size = [25000, 25000]
+    # sum(chunk_size) must be a divisor of num_waveforms + num_noises
+    chunk_size = [25, 25]
     
     """ Handling number of cores for task """
     # Used in MP and MPB dataset generation methods
@@ -92,13 +95,13 @@ class Default:
     # Create a new class for a different problem instead of changing this config
     sample_rate = 2048. # Hz
     # (20.0 seconds max + 2.0 seconds of noise padding) would be better
-    signal_length = 20.0  # seconds
+    signal_length = 1.0  # seconds
     # whiten_padding is also known as max_filter_duration in some modules
     whiten_padding = 5.0 # seconds (padding/2.0 on each side of signal_length)
     sample_length_in_s = signal_length + whiten_padding # seconds
     sample_length_in_num = round(sample_length_in_s * sample_rate)
     
-    # Error padding (too late/too early errors in time_slice after project_wave)
+    # Error padding (combatting too late/too early errors in time_slice after project_wave)
     # Setting this to 0.1 causes a (PSD, signal) delta_f mismatch error. Annoying.
     error_padding_in_s = 0.5
     error_padding_in_num = round(error_padding_in_s * sample_rate)
@@ -107,13 +110,20 @@ class Default:
     signal_approximant = 'IMRPhenomXPHM'
     reference_freq = 20.0 # Hz
     
+    """ PRIORS """
     prior_low_mass = 7.0 # Msun
     prior_high_mass = 50.0 # Msun
     prior_low_chirp_dist = 130.0
     prior_high_chirp_dist = 350.0
     
-    tc_inject_lower = 18.0 # seconds
-    tc_inject_upper = 18.2 # seconds
+    tc_inject_lower = 0.7 # seconds
+    tc_inject_upper = 0.9 # seconds
+
+    ### MODS ###
+    # Modifications to Dataset
+    # Possible mods: ('uniform_signal_duration', 'uniform_chirp_mass')
+    # NOTE: Set to None if not required
+    modification = None
     
     """ PSD Params """
     noise_low_freq_cutoff = 15.0 # Hz
@@ -131,13 +141,20 @@ class Default:
     psd_est_segment_length = 36. # in seconds
     psd_est_segment_stride = 18. # in seconds
     
+    ## To use a mix of real O3a noise and artificial noise created by
+    ## colouring Gaussian noise using PSDs estimated from real O3a noise
+    # Option used only for dataset == 4
+    mixed_noise = False
+    mix_ratio = 0.5
+    # Use D2/D3 PSDs for D4
+    use_d3_psds_for_d4 = True
+    
     """ PSD bad band blacking out for Datasets 2,3,4 """
-    blackout_max_ratio = 5.0
+    blackout_max_ratio = 2.0
     
     """ generate_data.py Noise Params (if used) """
     # Only used in dataset 2 and 3
     # TODO: Not implemented yet. Do not use.
-    # if use_example_psd == False
     filter_duration = 128.
     
     """ Multirate sampling params """
@@ -152,21 +169,25 @@ class Default:
     ###
     # Maximum possible signal length in the entire dataset
     # Defined in MLMDC1 as being the longest signal in the testing dataset
-    max_signal_length = 20.0 # s
+    max_signal_length = 1.0 # s
     # Conservative value for max length of ringdown in seconds
     # This ringdown section will be sampled at max possible sample rate
-    ringdown_leeway = 0.1 # s
+    ringdown_leeway = 0.05 # s
     # Seconds before merger to include in max possible sample rate
-    merger_leeway=0.2 # s
+    merger_leeway = 0.08 # s
     # f_ISCO is multiplied by this factor and used a starting sample freq. at merger
     # If this factor == 2.0, the sampling freq. will be at Nyquist limit
-    start_freq_factor=2.5
+    start_freq_factor = 2.5
     # The starting freq. will be reduced by this factor**(n) for n = [0, N], 'N'is num of bins
     # If facrtor==2.0, the sampling freq. is halved every time the signal freq. reduced by
     # a factor equal to fbin_reduction_factor
-    fs_reduction_factor=1.9
+    fs_reduction_factor = 1.8
     # Check value where signal freq. reduces by this factor
     # When this happens, that data idx is store in bins as one of the edges for MR-sampling
-    fbin_reduction_factor=2.0
+    fbin_reduction_factor = 2.0
+    # Removing corrupted samples on either end of MR sampled data
+    corrupted_len = 4
     # Storage bins (DO NOT CHANGE or DELETE)
-    dbins = None    
+    dbins = None
+    network_sample_length = None
+    _decimated_bins = None
