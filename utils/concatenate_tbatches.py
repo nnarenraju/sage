c85e08bbc01509660c31b5af2f5084ae2cab15fc
nnarenraju <nnarenraju@gmail.com>
2024-07-10 17:44:40 +0100

    Cleaning up Sage (Batch 11)

---
+++ utils/concatenate_tbatches.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Filename        = Foobar.py
+Description     = Lorem ipsum dolor sit amet
+
+Created on Thu Apr 14 12:37:59 2022
+
+__author__      = nnarenraju
+__copyright__   = Copyright 2022, ProjectName
+__credits__     = nnarenraju
+__license__     = MIT Licence
+__version__     = 0.0.1
+__maintainer__  = nnarenraju
+__email__       = nnarenraju@gmail.com
+__status__      = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+Github Repository: NULL
+
+Documentation: NULL
+
+"""
+
+# BUILT-IN
+import os
+import glob
+import json
+import argparse
+from collections import defaultdict
+# Using defaultdict to concatenate dicts with same key (loaded json)
+# Make dd with list as anything else might now be JSON hashable
+dd = defaultdict(list)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--batch-dir", type=str, default='/Users/nnarenraju/Desktop/dataset',
+                        help="Directory containing all trainable.json files and trainable data files")
+    parser.add_argument("--save-dir", type=str, default='dataset_trainable_batched',
+                        help="Dataset dir in the usual format containing all relevant data")
+    parser.add_argument("--file_name", type=str, default='trainable.json',
+                        help="Name of the final contatenated trainable.json file")
+    parser.add_argument("--silent", action='store_true')
+    
+    opts = parser.parse_args()
+    
+    
+    # Get all dirs required to make full dataset
+    data_dirs = glob.glob(os.path.join(opts.batch_dir, "dataset_5e4_20s_D1_Batch_*"))
+    
+    trainable_lookup = []
+    trainable_data = []
+    for data_dir in data_dirs:
+        # Search the batch dir and get all the trainable.json files
+        trainable_lookup.extend(glob.glob(os.path.join(data_dir, "*.json")))
+        # Look for trainable data within batch folder
+        trainable_data.extend(glob.glob(os.path.join(data_dir, "trainable_batched_dataset/*.hdf")))
+    
+    # Get all JSON file data
+    all_json_data = []
+    for json_file in trainable_lookup:
+        with open(json_file) as data_file:
+            all_json_data.append(json.load(data_file))
+    
+    # Concatenate all JSON data together into one trainable.json
+    for n, dic in enumerate(all_json_data):
+        for key, value in dic.items():
+            if key == "path":
+                value = trainable_data[n]
+            if key == "ids":
+                value = n
+            if key in ["target", "norm_tc", "distance", "mchirp"]:
+                if len(value) == 1:
+                    value = value[0]
+                if len(value) != 50000:
+                    dd['ids'].pop(-1)
+                    dd['path'].pop(-1)
+                    break
+            
+            dd[key].append(value)
+    
+    # Make a dataset directory structure similar to train.py
+    save_dir = os.path.join(opts.batch_dir, opts.save_dir)
+    os.makedirs(save_dir, exist_ok=False)
+    
+    # Save trainable.json in the correct location
+    save_path = os.path.join(save_dir, opts.file_name)
+    with open(save_path, 'w', encoding='utf-8') as fp:
+        json.dump(dd, fp)
+        
+    
+    
+    
+    
+    
+    
+    
+            
+    
+
---
+++ utils/plotter.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Filename        = Foobar.py
+Description     = Lorem ipsum dolor sit amet
+
+Created on Thu Apr 21 19:41:42 2022
+
+__author__      = nnarenraju
+__copyright__   = Copyright 2022, ProjectName
+__credits__     = nnarenraju
+__license__     = MIT Licence
+__version__     = 0.0.1
+__maintainer__  = nnarenraju
+__email__       = nnarenraju@gmail.com
+__status__      = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+Github Repository: NULL
+
+Documentation: NULL
+
+"""
+
+import os
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def _plotter(title, xlabel, ylabel, hists, labels, save_path):
+    plt.figure(figsize=(9.0, 6.0))
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    for data, label in zip(hists, labels):
+        plt.hist(data, bins=100, alpha=0.8, label=label)
+    plt.legend()
+    plt.savefig(save_path)
+    plt.close()
+
+
+def figure(title=""):
+    plt.rc('font', family='serif')
+    plt.rc('xtick', labelsize='medium')
+    plt.rc('ytick', labelsize='medium')
+    fig, axs = plt.subplots(1, figsize=(16.0+16.0, 14.0))
+    fig.suptitle(title, fontsize=28, y=0.92)
+    return fig, axs
+
+
+def _overplot(ax, x=None, y=None, xlabel="x-axis", ylabel="y-axis", ls='solid', 
+          label="", c=None, yscale='linear', xscale='linear'):
+    # Plotting type
+    if label != "":
+        ax.plot(x, y, ls=ls, c=c, linewidth=3.0, label=label)
+    else:
+        ax.plot(x, y, ls=ls, c=c, linewidth=3.0)
+    # Plotting params
+    ax.set_xscale(xscale)
+    ax.set_yscale(yscale)
+    ax.grid(True, which='both')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend()
+
+
+def overlay_plotter(overview_filepaths, roc_paths, roc_aucs, save_dir, run_names):
+    # Read diagnostic file
+    fig_loss, ax_loss = figure(title="Loss Curves")
+    fig_accr, ax_accr = figure(title="Accuracy Curves")
+    fig_roc, ax_roc  = figure(title="ROC Curves")
+    # colour map
+    numc = len(overview_filepaths)
+    cmap = ["#"+''.join([random.choice('ABCDEF0123456789') for _ in range(6)]) for _ in range(numc)]
+    
+    for n, overview_filepath in enumerate(overview_filepaths):
+        data = pd.read_csv(overview_filepath, sep="    ", engine='python')
+        
+        try:
+            # All data fields
+            epochs = data['epoch'] + 1.0
+            training_loss = data['tot_tloss']
+            validation_loss = data['tot_vloss']
+            training_accuracy = data['train_acc']
+            validation_accuracy = data['valid_acc']
+            
+            ## Loss Curves
+            _overplot(ax_loss, epochs, training_loss, label=run_names[n], ylabel='Avg Loss', xlabel='Epochs', c=cmap[n])
+            _overplot(ax_loss, epochs, validation_loss, ls='dashed', ylabel='Avg Loss', xlabel='Epochs', c=cmap[n])
+            
+            ## Accuracy Curves
+            _overplot(ax_accr, epochs, training_accuracy, label=run_names[n], ylabel='Avg Accuracy', xlabel='Epochs', c=cmap[n])
+            _overplot(ax_accr, epochs, validation_accuracy, ylabel='Avg Accuracy', xlabel='Epochs', ls='dashed', c=cmap[n])
+        
+        except:
+            continue
+    
+    ## Keep legends outside the main plot for the sake of clarity
+    # Shrink current axis by 20%
+    box1 = ax_loss.get_position()
+    box2 = ax_accr.get_position()
+    box3 = ax_roc.get_position()
+    # Alter boxsize
+    ax_loss.set_position([box1.x0, box1.y0, box1.width * 0.5, box1.height])
+    ax_accr.set_position([box2.x0, box2.y0, box2.width * 0.5, box2.height])
+    ax_roc.set_position([box3.x0, box3.y0, box3.width * 0.5, box3.height])
+    
+    # Put a legend to the right of the current axis
+    ax_loss.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax_accr.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax_roc.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    
+    fig_loss.savefig(os.path.join(save_dir, 'overlay_loss.png'))
+    fig_accr.savefig(os.path.join(save_dir, 'overlay_accuracy.png'))
+    plt.close(fig_loss)
+    plt.close(fig_accr)
+    
+    # Plotting the ROC overlay plot
+    # colour map
+    numc = len(roc_paths)
+    cmap = ["#"+''.join([random.choice('ABCDEF0123456789') for _ in range(6)]) for _ in range(numc)]
+    
+    for n, roc_path in enumerate(roc_paths):
+        fpr, tpr = np.load(roc_path)
+        auc = np.load(roc_aucs[n])
+        ## Loss Curves
+        # Log ROC Curve
+        _overplot(ax_roc, fpr, tpr, c=cmap[n], 
+              ylabel="True Positive Rate", xlabel="False Positive Rate", 
+              yscale='log', xscale='log', label=run_names[n]+'-AUC_{}'.format(np.around(auc, 3)))
+    
+    _overplot(ax_roc, [0, 1], [0, 1], label="Random Classifier", c='k', 
+              ylabel="True Positive Rate", xlabel="False Positive Rate", 
+              ls="dashed", yscale='log', xscale='log')
+    
+    fig_roc.savefig(os.path.join(save_dir, 'overlay_roc.png'))
+    plt.close(fig_roc)
+    
+
+def snr_plotter(snr_dir, nepochs):
+    snrs = np.loadtxt(snr_dir + '/snr_priors.csv')
+    title = 'SNR Histogram over nepochs = {} with augmentation'.format(nepochs)
+    xlabel = 'SNR'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(snr_dir, 'prior_SNR.png')
+    _plotter(title, xlabel, ylabel, [snrs], ['Augmented'], save_path)
+
+
+def debug_plotter(debug_dir):
+
+    distance_new = np.loadtxt(debug_dir + '/save_augment_distance_new.txt')
+    distance_old = np.loadtxt(debug_dir + '/save_augment_distance_old.txt')
+    title = 'Comparing the old and new distance priors'
+    xlabel = 'Distance'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_compare_distance.png')
+    _plotter(title, xlabel, ylabel, [distance_old, distance_new], ['Original', 'Augmented'], save_path)
+    
+    pol = np.loadtxt(debug_dir + '/save_augment_pol.txt')
+    title = 'Polarisation for 20 epochs of approx 1e4 signals each'
+    xlabel = 'Polarisation Angle'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_pol_angle.png')
+    _plotter(title, xlabel, ylabel, [pol], ['Pol Angle'], save_path)
+    
+    ra = np.loadtxt(debug_dir + '/save_augment_ra.txt')
+    title = 'RA for 20 epochs of approx 1e4 signals each'
+    xlabel = 'RA'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_ra.png')
+    _plotter(title, xlabel, ylabel, [ra], ['ra'], save_path)
+    
+    dec = np.loadtxt(debug_dir + '/save_augment_dec.txt')
+    title = 'DEC for 20 epochs of approx 1e4 signals each'
+    xlabel = 'DEC'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_dec.png')
+    _plotter(title, xlabel, ylabel, [dec], ['dec'], save_path)
+    
+    noise_slide_1 = np.loadtxt(debug_dir + '/save_augment_noise_shift_1.txt')
+    title = 'Noise Slide 1 for 20 epochs of approx 1e4 signals each'
+    xlabel = 'Noise Slide 1'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_noise_slide_1.png')
+    _plotter(title, xlabel, ylabel, [noise_slide_1], ['noise_slide_1'], save_path)
+    
+    noise_slide_2 = np.loadtxt(debug_dir + '/save_augment_noise_shift_2.txt')
+    title = 'Noise Slide 2 for 20 epochs of approx 1e4 signals each'
+    xlabel = 'Noise Slide 2'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_noise_slide_2.png')
+    _plotter(title, xlabel, ylabel, [noise_slide_2], ['noise_slide_2'], save_path)
+    
+    dchirp = np.loadtxt(debug_dir + '/save_augment_dchirp.txt')
+    title = 'Chirp Distance for 20 epochs of approx 1e4 signals each'
+    xlabel = 'Chirp Distance'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_dchirp.png')
+    _plotter(title, xlabel, ylabel, [dchirp], ['dchirp'], save_path)
+    
+    noise_idx = np.loadtxt(debug_dir + '/save_augment_train_random_noise_idx.txt')
+    title = 'Train Random noise idx for 20 epochs of approx 1e4 signals each'
+    xlabel = 'Train Random noise idx'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_train_noise_idx.png')
+    _plotter(title, xlabel, ylabel, [noise_idx], ['random_noise_idx'], save_path)
+    
+    noise_idx = np.loadtxt(debug_dir + '/save_augment_valid_random_noise_idx.txt')
+    title = 'Valid Random noise idx for 20 epochs of approx 1e4 signals each'
+    xlabel = 'Valid Random noise idx'
+    ylabel = 'Number of Occurences'
+    save_path = os.path.join(debug_dir, 'AUG_valid_noise_idx.png')
+    _plotter(title, xlabel, ylabel, [noise_idx], ['random_noise_idx'], save_path)
