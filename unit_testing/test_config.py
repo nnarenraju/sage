65143366c89ab8b05437e77f42a1129c9a8e5164
Narenraju Nagarajan <nnarenraju@gmail.com>
2024-07-15 12:28:37 +0100

    Adding config tests

--- unit_testing/test_config.py
+++ unit_testing/test_config.py
@@ -33,21 +33,35 @@ Tests included:
     9. Is num_workers available?
     10. If rescale_snr, is rescaled_snr_lower below the SNR wall?
     11. If rescale_snr, is rescaled_snr_upper too large?
-    12. 
-
-
+    12. Path to random noise slice exists?
+    13. Path to glitch augment should exist
+    14. Path to O3b noise should exist
+    15. Path to recolour PSDs should exist
+    16. If network io True, model should be in permitted models
+    17. Epoch testing files exist
+    18. All testing files exist
+    19. step_size for testing is not zero or too small
+    20. trigger threshold is not too large
 
 """
 
 import sys
 sys.path.insert(0, ".")
+
+import pytest
 import argparse
-from configs import *
+import configs
 
-class TestORChiDConfigs:
+from inspect import isclass
 
-    def __init__(self, config):
-        self.test_cfg = eval(opts.config)
+
+@pytest.fixture
+def read_config():
+    # Read config class from configs.py
+    classes = [x for x in dir(configs) if isclass(getattr(configs, x))]
+    return eval(config)
+
+
 
 
 
