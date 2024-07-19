ad45ab20f9507677bf5be482c6d02d32ff6f4940
nnarenraju <nnarenraju@gmail.com>
2024-07-19 13:04:01 +0100

    Updating task list to keep track of tmp files handled

--- configs.py
+++ configs.py
@@ -88,6 +88,10 @@ from ray.tune.schedulers import ASHAScheduler
 # TASKS
 # Cleanup prior modifications
 # All tmp files must be placed in a single location (eg. segments.csv)
+    # durs_H1_O3b_all_noise
+    # durs_L1_O3b_all_noise
+    # freq_psd.npy
+    #
 # code to produce all tmp files must be consolidated (add to utils)
 # code to download noise files for full experimentation must be consolidated (add to utils)
 # Change all debug folders to exist within export_dir
