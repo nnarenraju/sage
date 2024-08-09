1d5a4a33d6e17c1c796250ce1a055ea3945b27a7
nnarenraju <nnarenraju@gmail.com>
2024-08-09 22:22:58 +0100

    Doing a production run of fixed dataset ablation study

--- data_configs.py
+++ data_configs.py
@@ -274,16 +274,12 @@ class DefaultOTF:
     # This seed is used to generate the priors
     seed = 110798
     # Fix epoch seeds for lowering dataset variation
-    fix_epoch_seeds = False
+    fix_epoch_seeds = True
 
     """ OTF Params """
-    #num_training_samples = 2_000_000
-    #num_validation_samples = 500_000
-    #num_auxilliary_samples = 125_000
-
-    num_training_samples = 100
-    num_validation_samples = 50
-    num_auxilliary_samples = 10
+    num_training_samples = 2_000_000
+    num_validation_samples = 500_000
+    num_auxilliary_samples = 125_000
     
     """ Signal Params """
     ## these params may be used if make_dataset == False
