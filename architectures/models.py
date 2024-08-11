f8ee529c1453a904e9db155340deb8b76b81034a
nnarenraju <nnarenraju@gmail.com>
2024-08-11 20:43:15 +0100

    Cleaned up resnet1D model

--- architectures/models.py
+++ architectures/models.py
@@ -38,6 +38,7 @@ from architectures.zoo.res2net_v1b import res2net101_v1b_26w_4s, res2net50_v1b_2
 from architectures.zoo.osnet1d import osnet_ain_custom as osnet1d
 from architectures.zoo.kaggle import ConvBlock, _initialize_weights
 from architectures.frontend import MSFeatureExtractor, MultiScaleBlock
+from architectures.zoo.resnet1d import resnet50, resnet101
 
 # Code Review
 from utils.decorators import unreviewed_model
@@ -762,34 +763,14 @@ class KappaModel_ResNet1D(torch.nn.Module):
     """
     Kappa-type Model PE Architecture with ResNet1D
     
-    Description - consists of a 2-channel ConvBlock frontend and a Timm model backend
-                  this Model-type can be used to test the Kaggle architectures
-    
-    Parameters
-    ----------
-    model_name  = 'simple' : string
-        Simple NN model name for Frontend. Save model with this name as attribute.
-    pretrained  = False : Bool
-        Pretrained option for saved models
-        If True, weights are stored under the model_name in saved_models dir
-        If model name already exists, throws an error (safety)
-    in_channels = 2 : int
-        Number of input channels (number of detectors)
-    out_channels = 2 : int
-        Number of output channels (signal, noise)
-    store_device = 'cpu' : str
-        Storage device for network (NOTE: make sure data is also stored in the same device)
-    weights_path = '' : str
-        Absolute path to the weights.pt file. Used when pretrained == True
+    Description - consists of a ResNet1D backend
         
     """
 
     def __init__(self, 
-                 model_name='smallnet',
-                 timm_params: dict = {'model_name': 'resnet50', 'pretrained': False, 'in_chans': 2, 'drop_rate': 0.25},
+                 model_name='resnet1d',
+                 resnet_size: int = 50,
                  norm_layer: str = 'instancenorm',
-                 _input_length: int = 4254,
-                 _decimated_bins = None,
                  store_device: str = 'cpu',
                  **kwargs):
         
@@ -799,18 +780,14 @@ class KappaModel_ResNet1D(torch.nn.Module):
         self.store_device = store_device
         self.norm_layer = norm_layer
         
-        """ Frontend """
-        # Pretrained model is for 3-channels. We use 2 channels.
-        self.backend = timm.create_model(**timm_params)
-        
-        # reset_classifier edits the number of outputs that Timm produces
-        # The following is set if we need a two-class output from Timm
-        # This can be set to a larger value and connected to a linear layer
-        self.backend.reset_classifier(1)
+        """ 1D ResNet """
+        if resnet_size == 50:
+            self.resnet = resnet50(num_classes=512)
+        elif resnet_size = 101:
+            self.resnet = resnet101(num_classes=512)
         
         """ Mods """
         # Manipulation layers
-        self.avg_pool_2d = nn.AdaptiveAvgPool2d((1, 1))
         self.batchnorm = nn.BatchNorm1d(2)
         self.instancenorm = nn.InstanceNorm1d(2, affine=True)
         self.flatten_d1 = nn.Flatten(start_dim=1)
@@ -821,18 +798,17 @@ class KappaModel_ResNet1D(torch.nn.Module):
         self.softmax = torch.nn.Softmax(dim=1)
         self.ReLU = nn.ReLU()
         self.Tanh = nn.Tanh()
-        self.upsample = nn.Upsample(scale_factor=self.upsample_factor, mode='bicubic')
         
         ## Convert network into given dtype and store in proper device 
         # Primary outputs
         self.signal_or_noise = nn.Linear(512, 1)
-        self.coalescence_time = nn.Linear(512, 2)
-        self.chirp_distance = nn.Linear(512, 2)
-        self.chirp_mass = nn.Linear(512, 2)
-        self.distance = nn.Linear(512, 2)
-        self.mass_ratio = nn.Linear(512, 2)
-        self.inv_mass_ratio = nn.Linear(512, 2)
-        self.snr = nn.Linear(512, 2)
+        self.coalescence_time = nn.Linear(512, 1)
+        self.chirp_distance = nn.Linear(512, 1)
+        self.chirp_mass = nn.Linear(512, 1)
+        self.distance = nn.Linear(512, 1)
+        self.mass_ratio = nn.Linear(512, 1)
+        self.inv_mass_ratio = nn.Linear(512, 1)
+        self.snr = nn.Linear(512, 1)
         # Mod layers
         self.signal_or_noise.to(dtype=data_type, device=self.store_device)
         self.coalescence_time.to(dtype=data_type, device=self.store_device)
@@ -845,15 +821,9 @@ class KappaModel_ResNet1D(torch.nn.Module):
 
         # Manipulation layers
         self.batchnorm.to(dtype=data_type, device=self.store_device)
-        self.dain.to(dtype=data_type, device=self.store_device)
-        self.layernorm.to(dtype=data_type, device=self.store_device)
-        #self.layernorm_cnn.to(dtype=data_type, device=self.store_device)
         self.instancenorm.to(dtype=data_type, device=self.store_device)
         # Main layers
-        self._det1.to(dtype=data_type, device=self.store_device)
-        self._det2.to(dtype=data_type, device=self.store_device)
-        self.frontend = {'det1': self._det1, 'det2': self._det2}
-        self.backend.to(dtype=data_type, device=self.store_device)
+        self.resnet.to(dtype=data_type, device=self.store_device)
     
     # x.shape: (batch size, wave channel, length of wave)
     def forward(self, x):
@@ -862,16 +832,9 @@ class KappaModel_ResNet1D(torch.nn.Module):
             normed = self.batchnorm(x)
         elif self.norm_layer == 'instancenorm':
             normed = self.instancenorm(x)
-        
-        # Upsampling of input normed data
-        if self.upsample_factor != 1.0:
-            normed = torch.cat([self.upsample(normed[:, 0:1]), self.upsample(normed[:, 1:2])], dim=1)
 
-        # Conv Backend
-        cnn_output = torch.cat([self.frontend['det1'](normed[:, 0:1]), self.frontend['det2'](normed[:, 1:2])], dim=1)
-
-        # Timm Frontend
-        out = self.backend(cnn_output) # (batch_size, 512)
+        # Resnet1D
+        out = self.resnet(normed) # (batch_size, 512)
         out = self.flatten_d1(self.avg_pool_1d(out))
         ## Output necessary params
         raw = self.flatten_d0(self.signal_or_noise(out))
@@ -879,49 +842,32 @@ class KappaModel_ResNet1D(torch.nn.Module):
 
         ## Parameter Estimation
         # Time of Coalescence
-        tc_ = self.coalescence_time(out)
-        tc = self.flatten_d0(tc_[:,0])
+        tc = self.flatten_d0(self.coalescence_time(out))
         norm_tc = self.sigmoid(tc)
-        tc_var = self.Tanh(self.flatten_d0(tc_[:,1]))
         # Chirp Distance
-        dchirp_ = self.chirp_distance(out)
-        dchirp = self.flatten_d0(dchirp_[:,0])
+        dchirp = self.flatten_d0(self.chirp_distance(out))
         norm_dchirp = self.sigmoid(dchirp)
-        dchirp_var = self.flatten_d0(dchirp_[:,1])
         # Chirp Mass
-        mchirp_ = self.chirp_mass(out)
-        mchirp = self.flatten_d0(mchirp_[:,0])
+        mchirp = self.flatten_d0(self.chirp_mass(out))
         norm_mchirp = self.sigmoid(mchirp)
-        mchirp_var = self.Tanh(self.flatten_d0(mchirp_[:,1]))
         # Distance
-        dist_ = self.distance(out)
-        dist = self.flatten_d0(dist_[:,0])
+        dist = self.flatten_d0(self.distance(out))
         norm_dist = self.sigmoid(dist)
-        dist_var = self.flatten_d0(dist_[:,1])
         # Mass Ratio
-        q_ = self.mass_ratio(out)
-        q = self.flatten_d0(q_[:,0])
+        q = self.flatten_d0(self.mass_ratio(out))
         norm_q = self.sigmoid(q)
-        q_var = self.flatten_d0(q_[:,1])
         # Inverse Mass Ratio
-        invq_ = self.inv_mass_ratio(out)
-        invq = self.flatten_d0(invq_[:,0])
+        invq = self.flatten_d0(self.inv_mass_ratio(out))
         norm_invq = self.sigmoid(invq)
-        invq_var = self.flatten_d0(invq_[:,1])
         # SNR
-        snr_ = self.snr(out)
-        snr = self.flatten_d0(snr_[:,0])
+        snr = self.flatten_d0(self.snr(out))
         norm_snr = self.sigmoid(snr)
-        snr_var = self.flatten_d0(snr_[:,1])
         
         # Return ouptut params (pred_prob, raw, cnn_output, pe_params)
-        return {'raw': raw, 'pred_prob': pred_prob, 'cnn_output': cnn_output,
+        return {'raw': raw, 'pred_prob': pred_prob, 'input': x,
                 'norm_tc': norm_tc, 'norm_dchirp': norm_dchirp, 'norm_mchirp': norm_mchirp,
                 'norm_dist': norm_dist, 'norm_q': norm_q, 'norm_invq': norm_invq, 'norm_snr': norm_snr,
-                'tc': tc, 'dchirp': dchirp, 'mchirp': mchirp, 'dist': dist, 'q': q, 'invq': invq, 'snr': snr,
-                'norm_tc_sigma': tc_var, 'norm_mchirp_sigma': mchirp_var, 'norm_snr_sigma': snr_var,
-                'norm_q_sigma': q_var, 'norm_invq_sigma': invq_var, 'norm_dist_sigma': dist_var,
-                'norm_dchirp_sigma': dchirp_var, 'input': x, 'normed': normed}
+                'tc': tc, 'dchirp': dchirp, 'mchirp': mchirp, 'dist': dist, 'q': q, 'invq': invq, 'snr': snr}
 
 
 
