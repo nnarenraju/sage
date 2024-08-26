170b9dfc0643cf41bd0ff9234a23ea0026963ed5
nnarenraju <nnarenraju@gmail.com>
2024-08-26 15:01:27 +0100

    Running condor submit via stable sage env

---
+++ sage_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                                                                                                                                                                                                                    
+# A script to set up a specific environment and to run a python script in it                                                                                                                                      
+# 
+
+# Activate the virtual env
+source ~/venv/sage/bin/activate
+python3 $@ 
