d6469cfaf8cc8d5fa16fe692f786db17545f053b
nnarenraju <nnarenraju@gmail.com>
2024-08-31 23:01:53 +0100

    Running Rooster runs on 2 second sample lengths

--- data_configs.py
+++ data_configs.py
@@ -280,7 +280,7 @@ class DefaultOTF:
     # Create a new class for a different problem instead of changing this config
     sample_rate = 2048. # Hz
     # (20.0 seconds max + 2.0 seconds of noise padding) would be better
-    signal_length = 12.0 # seconds
+    signal_length = 2.0 # seconds (CHANGE THIS BACK TO 12!!!)-------------------------------------------- !!!LOOK HERE!!!
     # Noise padding after ringdown
     # Signal will be placed based on requested noise pad and post fudge factor
     # if signal length is not sufficient for longest possible signal, error occurs.
