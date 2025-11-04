4a4cc9761dd5d4b04c5d80928f60393c778ec585
nnarenraju <nnarenraju@gmail.com>
2025-11-04 06:11:05 +0000

    Major refactoring of Sage

--- cleanup.md
+++ cleanup.md
@@ -1,2 +1,3 @@
 Tasks to do for Sage cleanup:
-1. (03/11/2025) Splitting and cleaning up manual.py
+1. (03/11/2025) Moving things around into a good enough file structure
+2. (04/11/2025) Making sure that the new file structure still lets the code run
---
+++ sage/exec/generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : generator.py
+Description     : Short description of the file
+
+Created on 2025-11-04 07:08:44
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2025, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
+
+def main():
+    raise NotImplementedError("Dataset generation not yet implemented.")
+
+if __name__ == "__main__":
+    main()
