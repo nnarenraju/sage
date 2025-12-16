c1a5f1b75022b5a0d5a48b6d63dfce152578e6a2
nnarenraju <nnarenraju@gmail.com>
2025-12-16 19:13:36 +0000

    Added code to estimate PSDs from monolithic data file

---
+++ sage/data/download/get_psds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : get_psds.py
+Description     : Short description of the file
+
+Created on 2025-12-16 15:44:10
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2025, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
+
+# Packages
+
+
+class EstimatePSD:
+
+    class __init__(self):
+        pass
