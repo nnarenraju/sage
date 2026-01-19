46e7ce25cd88abb60baa3289de2efbb84b5d0429
nnarenraju <nnarenraju@gmail.com>
2026-01-19 22:37:48 +0000

    Calling all noise generation classes

---
+++ sage/data/noise/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : __init__.py
+Description     : Short description of the file
+
+Created on 2026-01-19 23:28:02
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
+
+from .real_noise import GenerateRealNoise
+
+__all__ = [
+    "GenerateRealNoise",
+]
