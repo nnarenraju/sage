85fea6a735327bd48f62df7c65c9895502d57231
nnarenraju <nnarenraju@gmail.com>
2026-01-19 22:16:08 +0000

    Added two new noise generation methods

---
+++ sage/data/noise/glitch_noise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : glitch_noise.py
+Description     : Short description of the file
+
+Created on 2026-01-19 21:51:33
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
