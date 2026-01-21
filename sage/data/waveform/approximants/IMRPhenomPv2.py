8af00bc29946dea3ee91fa294873c61ff243ea1d
nnarenraju <nnarenraju@gmail.com>
2026-01-21 05:26:47 +0000

    Started IMRPhenomPv2 main code

---
+++ sage/data/waveform/approximants/IMRPhenomPv2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : IMRPhenomPv2.py
+Description     : Short description of the file
+
+Created on 2026-01-21 05:26:04
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
