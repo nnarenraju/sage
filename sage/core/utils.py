5c30eec5728e9f389b8ed481864442d32e03cd5c
nnarenraju <nnarenraju@gmail.com>
2026-01-21 05:25:48 +0000

    Added custom torch interp

--- sage/core/utils.py
+++ sage/core/utils.py
@@ -24,6 +24,7 @@ Documentation: NULL
 """
 
 # Packages
+import torch
 import numpy as np
 
 
@@ -45,3 +46,66 @@ def ensure_1d(x):
     if x.ndim != 1:
         raise ValueError("Input must be 1D")
     return x
+
+
+def torch_interp(x, xp, fp):
+    """
+    1D linear interpolation like jnp.interp.
+    x:  Tensor (...,)
+    xp: Tensor (N,) increasing
+    fp: Tensor (N,)
+    """
+    # indices where elements should be inserted
+    idx = torch.searchsorted(xp, x, right=True)
+
+    idx = torch.clamp(idx, 1, xp.numel() - 1)
+
+    x0 = xp[idx - 1]
+    x1 = xp[idx]
+    y0 = fp[idx - 1]
+    y1 = fp[idx]
+
+    slope = (y1 - y0) / (x1 - x0)
+    return y0 + slope * (x - x0)
+
+
+class Normalise:
+    """
+    Normalise the variable using known bounds
+
+        For example, norm_tc = (tc - min_val)/(max_val - min_val)
+        The values of max_val and min_val are provided
+        to the class. obj.norm can be called during
+        data generation to get normalised values of tc, if needed.
+
+    """
+
+    def __init__(self, min_val, max_val):
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def norm(self, val):
+        # Return normalised value in [0, 1]
+        return (val - self.min_val) / (self.max_val - self.min_val)
+
+    def unnorm(self, val):
+        # Return unnormalised value in original scale
+        return (val * (self.max_val - self.min_val)) + self.min_val
+
+
+class Standardise:
+    """
+    Standardise the variable to zero mean and unit variance
+    """
+
+    def __init__(self, min_val, max_val):
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def norm(self, val):
+        # Return normalised value in [0, 1]
+        return (val - self.min_val) / (self.max_val - self.min_val)
+
+    def unnorm(self, val):
+        # Return unnormalised value in original scale
+        return (val * (self.max_val - self.min_val)) + self.min_val
