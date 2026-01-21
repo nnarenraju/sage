84b9ad0eddb928f1640f1ec36b366cecee08c1e7
nnarenraju <nnarenraju@gmail.com>
2026-01-21 05:25:33 +0000

    Added typing module

---
+++ sage/core/typing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : typing.py
+Description     : Short description of the file
+
+Created on 2026-01-21 06:18:56
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
+
+from typing import Union
+import torch
+
+Array = Union[torch.Tensor]
