03d7e9823c00dfdc363b537987ac446809b52fe1
nnarenraju <nnarenraju@gmail.com>
2026-01-22 20:58:36 +0000

    Converted from JAX to torch

--- sage/data/waveform/approximants/IMRPhenomPv2.py
+++ sage/data/waveform/approximants/IMRPhenomPv2.py
@@ -24,27 +24,25 @@ Documentation: NULL
 """
 
 
-from .IMRPhenomD import Phase as PhDPhase
-from .IMRPhenomD import Amp as PhDAmp
-from .IMRPhenomD_utils import get_coeffs
-
-from .IMRPhenomPv2_utils import *
-from .IMRPhenomD_utils import *
-
 # Packages
+import torch
 
 
 # LOCAL
 from sage.core.constants import GM
-from sage.core.typing import TorchArray
 from sage.core.conversions import mchirp_eta_to_m1_m2
+
+from .IMRPhenomD_utils import get_coeffs
+from .IMRPhenomD import Phase as PhDPhase
+from .IMRPhenomD import Amp as PhDAmp
+from .IMRPhenomPv2_utils import (
+    WignerdCoefficients,
+)
 
 
 def PhenomPCoreTwistUp(
     fHz,
     hPhenom,
-    # phase,
-    # Amp,
     eta,
     chi1_l,
     chi2_l,
@@ -59,8 +57,8 @@ def PhenomPCoreTwistUp(
     assert Y2m is not None
 
     # here it is used to be LAL_MTSUN_SI
-    f = fHz * gt * M  # Frequency in geometric units
-    q = (1.0 + jnp.sqrt(1.0 - 4.0 * eta) - 2.0 * eta) / (2.0 * eta)
+    f = fHz * GM * M  # Frequency in geometric units
+    q = (1.0 + torch.sqrt(1.0 - 4.0 * eta) - 2.0 * eta) / (2.0 * eta)
     m1 = 1.0 / (1.0 + q)  # Mass of the smaller BH for unit total mass M=1.
     m2 = q / (1.0 + q)  # Mass of the larger BH for unit total mass M=1.
     Sperp = chip * (
@@ -70,8 +68,8 @@ def PhenomPCoreTwistUp(
 
     SL = chi1_l * m1 * m1 + chi2_l * m2 * m2  # Dimensionfull aligned spin.
 
-    omega = jnp.pi * f
-    logomega = jnp.log(omega)
+    omega = torch.pi * f
+    logomega = torch.log(omega)
     omega_cbrt = (omega) ** (1 / 3)
     omega_cbrt2 = omega_cbrt * omega_cbrt
 
@@ -101,40 +99,31 @@ def PhenomPCoreTwistUp(
     sBetah3 = sBetah2 * sBetah
     sBetah4 = sBetah3 * sBetah
 
-    # d2 = jnp.array(
-    #     [
-    #         sBetah4,
-    #         2 * cBetah * sBetah3,
-    #         jnp.sqrt(6) * sBetah2 * cBetah2,
-    #         2 * cBetah3 * sBetah,
-    #         cBetah4,
-    #     ]
-    # )
-    Y2mA = jnp.array(Y2m)  # need to pass Y2m in a 5-component list
+    Y2mA = torch.tensor(Y2m)  # need to pass Y2m in a 5-component list
     hp_sum = 0
     hc_sum = 0
 
-    cexp_i_alpha = jnp.exp(1j * alpha)
+    cexp_i_alpha = torch.exp(1j * alpha)
     cexp_2i_alpha = cexp_i_alpha * cexp_i_alpha
     cexp_mi_alpha = 1.0 / cexp_i_alpha
     cexp_m2i_alpha = cexp_mi_alpha * cexp_mi_alpha
     T2m = (
         cexp_2i_alpha * cBetah4 * Y2mA[0]
         - cexp_i_alpha * 2 * cBetah3 * sBetah * Y2mA[1]
-        + 1 * jnp.sqrt(6) * sBetah2 * cBetah2 * Y2mA[2]
+        + 1 * torch.sqrt(6) * sBetah2 * cBetah2 * Y2mA[2]
         - cexp_mi_alpha * 2 * cBetah * sBetah3 * Y2mA[3]
         + cexp_m2i_alpha * sBetah4 * Y2mA[4]
     )
     Tm2m = (
-        cexp_m2i_alpha * sBetah4 * jnp.conjugate(Y2mA[0])
-        + cexp_mi_alpha * 2 * cBetah * sBetah3 * jnp.conjugate(Y2mA[1])
-        + 1 * jnp.sqrt(6) * sBetah2 * cBetah2 * jnp.conjugate(Y2mA[2])
-        + cexp_i_alpha * 2 * cBetah3 * sBetah * jnp.conjugate(Y2mA[3])
-        + cexp_2i_alpha * cBetah4 * jnp.conjugate(Y2mA[4])
+        cexp_m2i_alpha * sBetah4 * torch.conj(Y2mA[0])
+        + cexp_mi_alpha * 2 * cBetah * sBetah3 * torch.conj(Y2mA[1])
+        + 1 * torch.sqrt(6) * sBetah2 * cBetah2 * torch.conj(Y2mA[2])
+        + cexp_i_alpha * 2 * cBetah3 * sBetah * torch.conj(Y2mA[3])
+        + cexp_2i_alpha * cBetah4 * torch.conj(Y2mA[4])
     )
     hp_sum = T2m + Tm2m
     hc_sum = 1j * (T2m - Tm2m)
-    eps_phase_hP = jnp.exp(-2j * epsilon) * hPhenom / 2.0
+    eps_phase_hP = torch.exp(-2j * epsilon) * hPhenom / 2.0
 
     hp = eps_phase_hP * hp_sum
     hc = eps_phase_hP * hc_sum
@@ -153,8 +142,8 @@ def PhenomPOneFrequency(
     # These are the parametrs that go into the waveform generator
     # Note that JAX does not give index errors, so if you pass in the
     # the wrong array it will behave strangely
-    norm = 2.0 * jnp.sqrt(5.0 / (64.0 * jnp.pi))
-    theta_ripple = jnp.array([m1, m2, chi1, chi2])
+    norm = 2.0 * torch.sqrt(5.0 / (64.0 * torch.pi))
+    theta_ripple = torch.array([m1, m2, chi1, chi2])
 
     phase = PhDPhase(fs, theta_ripple, coeffs, transition_freqs)
     Dphi = lambda f: -PhDPhase(f, theta_ripple, coeffs, transition_freqs)
@@ -163,7 +152,7 @@ def PhenomPOneFrequency(
     Amp = PhDAmp(fs, theta_ripple, coeffs, transition_freqs, D=dist_mpc) / norm
 
     # phase -= 2. * phic; # line 1316 ???
-    hPhenom = Amp * (jnp.exp(-1j * phase))
+    hPhenom = Amp * (torch.exp(-1j * phase))
     return hPhenom, Dphi
 
 
@@ -202,7 +191,7 @@ def gen_IMRPhenomPv2(
     chi_eff = (m1 * chi1_l + m2 * chi2_l) / M
     chil = (1.0 + q) / q * chi_eff
     eta = m1 * m2 / (M * M)
-    m_sec = M * gt
+    m_sec = M * GM
     piM = jnp.pi * m_sec
 
     omega_ref = piM * f_ref
@@ -270,7 +259,7 @@ def gen_IMRPhenomPv2(
     # )
     t0 = jax.grad(phi_IIb)(f_RD) / (2 * jnp.pi)
     phase_corr = jnp.cos(2 * jnp.pi * fs * (t0)) - 1j * jnp.sin(2 * jnp.pi * fs * (t0))
-    M_s = (m1 + m2) * gt
+    M_s = (m1 + m2) * GM
     phase_corr_tc = jnp.exp(-1j * fs * M_s * tc)
     hp *= phase_corr * phase_corr_tc
     hc *= phase_corr * phase_corr_tc
@@ -290,7 +279,7 @@ def gen_IMRPhenomPv2_hphc(f: Array, params: Array, f_ref: float):
     """
     Mc = params[0]
     eta = params[1]
-    m1, m2 = Mc_eta_to_ms(jnp.array([Mc, eta]))
+    m1, m2 = mchirp_eta_to_m1_m2(jnp.array([Mc, eta]))
     m1m2params = jnp.array(
         [
             m1,
