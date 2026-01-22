78e0072b82ffe9bc624d391b7915f09bd841e309
nnarenraju <nnarenraju@gmail.com>
2026-01-22 09:40:38 +0000

    Added IMRPhonomD

---
+++ sage/data/waveform/approximants/IMRPhenomD_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : IMRPhenomD_utils.py
+Description     : Short description of the file
+
+Created on 2026-01-22 10:38:55
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
