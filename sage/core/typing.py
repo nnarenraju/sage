f1d52aeb48b8f58e1756c72b443eb6cf2f7ebbad
nnarenraju <nnarenraju@gmail.com>
2026-01-22 09:23:30 +0000

    Added types into typing

--- sage/core/typing.py
+++ sage/core/typing.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-Filename        : typing.py
+Filename        : base_entities.py
 Description     : Short description of the file
 
-Created on 2026-01-21 06:18:56
+Created on 2025-11-06 17:57:46
 
 __author__        = Narenraju Nagarajan
-__copyright__     = Copyright 2026, ProjectName
+__copyright__     = Copyright 2025, ProjectName
 __license__       = MIT Licence
 __version__       = 0.0.1
 __maintainer__    = Narenraju Nagarajan
@@ -23,7 +23,24 @@ Documentation: NULL
 
 """
 
-from typing import Union
+# General
 import torch
+import numpy as np
 
-Array = Union[torch.Tensor]
+from typing import Union
+
+
+# Speed is key
+TorchArray = Union[torch.Tensor]
+
+# Structured dtype for segment results
+SEGMENT_DTYPE = np.dtype(
+    [
+        ("detector", "U2"),  # Detector, e.g. 'H1'
+        ("flag", "U10"),  # Data quality flag, e.g. 'H1_DATA'
+        ("start_time", "f8"),  # Start time (float)
+        ("end_time", "f8"),  # End time (float)
+        ("observing_run", "U10"),  # Run name
+        ("segments", "O"),  # Segment list or sub-array
+    ]
+)
