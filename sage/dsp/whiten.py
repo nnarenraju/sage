488d07d7db2a3d316518c0f1e9d54fb3aea2503d
nnarenraju <nnarenraju@gmail.com>
2026-02-11 21:57:58 +0000

    Added compile friendly fiducial PSD whitening

--- sage/dsp/whiten.py
+++ sage/dsp/whiten.py
@@ -22,105 +22,58 @@ Documentation: NULL
 
 """
 
+# Packages
+import torch
 
-class Whiten:
+
+class FiducialWhitening(torch.nn.Module):
+    """
+    Whiten frequency-domain strain using fixed fiducial PSDs.
+
+    Input:
+        X_fd: (B, D, F) complex64
+
+    Output:
+        x_td_white: (B, D, T) float32
+    """
 
     def __init__(
         self,
-        trunc_method="hann",
-        remove_corrupted=True,
-        estimated=False,
-        whitening_psd_dir=None,
+        fiducial_psds: torch.Tensor,  # (D, F) float32
+        seq_len: int,
+        sample_rate: float,
+        eps: float = 1e-12,
     ):
-        super().__init__(always_apply)
-        self.trunc_method = trunc_method
-        self.remove_corrupted = remove_corrupted
-        self.estimated = estimated
-        if whitening_psd_dir != None:
-            # Store the PSD files here in RAM. This reduces the overhead when whitening.
-            # Read all psds in the data_dir and store then as FrequencySeries
-            psds = {}
-            H1_dir = os.path.join(whitening_psd_dir, "H1")
-            L1_dir = os.path.join(whitening_psd_dir, "L1")
-            psd_file_H1 = glob.glob(os.path.join(H1_dir, "*.hdf"))
-            psd_file_L1 = glob.glob(os.path.join(L1_dir, "*.hdf"))
-            for psd_file in psd_file_H1:
-                psd_data = load_frequencyseries(psd_file)
-                # Store PSD data into lookup dict
-                psds["H1"] = psd_data
-            for psd_file in psd_file_L1:
-                psd_data = load_frequencyseries(psd_file)
-                # Store PSD data into lookup dict
-                psds["L1"] = psd_data
-            self.whitening_psd = [psds["H1"], psds["L1"]]
-        else:
-            self.whitening_psd = whitening_psd_dir
+        super().__init__()
 
-    def estimate_psd(self, data_cfg, delta_f, max_filter_len):
-        ### Estimate the PSD
-        delta_t = 1.0 / 2048.0
-        seg_len = int(0.5 / delta_t)
-        seg_stride = int(seg_len / 2)
-        pure_noise = TimeSeries(pure_noise, delta_t=1.0 / data_cfg.sample_rate)
-        psd = welch(pure_noise, seg_len=seg_len, seg_stride=seg_stride)
-        psd = interpolate(psd, delta_f)
-        psd = inverse_spectrum_truncation(
-            psd,
-            max_filter_len=max_filter_len,
-            low_frequency_cutoff=data_cfg.signal_low_freq_cutoff,
-            trunc_method=self.trunc_method,
-        )
-        return psd
+        self.seq_len = seq_len
+        self.sample_rate = sample_rate
+        self.eps = eps
 
-    def whiten(self, signal, psd, data_cfg):
-        """Return a whitened time series"""
-        # Convert signal to Timeseries object
-        signal = TimeSeries(signal, delta_t=1.0 / data_cfg.sample_rate)
-        # Filter length for inverse spectrum truncation
-        max_filter_len = int(round(data_cfg.whiten_padding * data_cfg.sample_rate))
+        # Frequency resolution
+        delta_f = sample_rate / seq_len
 
-        ## Manipulate PSD for usage in whitening
-        ## Interpolation is probably not required as the psds are created based on signal len anyway
-        # Calculating delta_f of signal and providing that to the PSD interpolation method
-        delta_f = data_cfg.delta_f
-        # Interpolate the PSD to the required delta_f
-        # NOTE: This may or may not be required (enable if there is a discrepancy in delta_f)
-        # Possible bug: It is possible that the sample lengths are not consistent in Dataloader
-        psd = interpolate(psd, delta_f)
+        # Whitening factor:
+        # sqrt(2 * delta_f) / sqrt(PSD)
+        # (factor 2 because one-sided PSD)
+        whitening = torch.sqrt(2.0 * delta_f / (fiducial_psds + eps))  # (D, F)
 
-        ## Whitening
-        # Whiten the data by the asd
-        if self.estimated:
-            raise NotImplementedError(
-                "PSD Estimation method not working at the moment!"
-            )
+        # Register as buffer for compile friendliness
+        self.register_buffer("whitening", whitening)  # (D, F)
 
-        # Interpolate and smooth to the desired corruption length
-        psd = inverse_spectrum_truncation(
-            psd,
-            max_filter_len=max_filter_len,
-            low_frequency_cutoff=data_cfg.signal_low_freq_cutoff,  # used to be signal low freq cutoff
-            trunc_method=self.trunc_method,
+    def forward(self, X_fd: torch.Tensor) -> torch.Tensor:
+        """
+        X_fd: (B, D, F) complex64
+        """
+
+        # Apply whitening in FD
+        X_white = X_fd * self.whitening.unsqueeze(0)
+
+        # Back to time domain
+        x_td_white = torch.fft.irfft(
+            X_white,
+            n=self.seq_len,
+            dim=-1,
         )
 
-        # NOTE: Factor of dt not taken into account. Since layernorm takes care of standardisation,
-        # we don't necessarily need to include this. After decorrelation, the diagonal matrix
-        # values will not be 1 but some other value dependant on the signal input.
-        white = (signal.to_frequencyseries(delta_f=delta_f) / psd**0.5).to_timeseries()
-
-        if self.remove_corrupted:
-            white = white[
-                int(max_filter_len / 2) : int(len(white) - max_filter_len / 2)
-            ]
-
-        return white
-
-    def apply(self, y: np.ndarray, channel: int, special: dict):
-        ## Whitening using approximate PSD
-        # Use a fixed provided PSD
-        if self.whitening_psd != None:
-            psd = self.whitening_psd[channel]
-        else:
-            psd = special["psds"][channel]
-        # Whiten sample and return results
-        return self.whiten(y, psd, special["data_cfg"])
+        return x_td_white
