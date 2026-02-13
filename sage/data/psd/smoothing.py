7141a2e2072753d245620aa5835080d1e659e37b
nnarenraju <nnarenraju@gmail.com>
2026-02-13 15:18:59 +0000

    Added safety and OOP concepts

--- sage/data/psd/smoothing.py
+++ sage/data/psd/smoothing.py
@@ -29,59 +29,84 @@ import numpy as np
 from scipy.interpolate import UnivariateSpline
 
 
-def smooth_psd_log_spline(
-    freqs,
-    psd,
-    upweight_regions=None,
-    smooth_factor=None,
-    return_coeffs=False,
-):
-    """
-    Smooth a noisy PSD estimate using a spline in log-log space.
+class LogSplineSmoothing:
 
-    Parameters
-    ----------
-    freqs : (F,) array
-        Frequency array (must be > 0).
-    psd : (F,) array
-        PSD values (must be > 0).
-    smooth_factor : float or None
-        Smoothing strength. Larger = smoother.
-        If None, an automatic heuristic is used.
+    def __init__(
+        self,
+        smooth_factor=None,
+        upweight_regions=None,
+        return_coeffs=False,
+        noise_low_frequency_cutoff=15.0,
+    ):
+        self.smooth_factor = smooth_factor
+        self.upweight_regions = upweight_regions
+        self.return_coeffs = return_coeffs
+        self.noise_low_frequency_cutoff = noise_low_frequency_cutoff
 
-    Returns
-    -------
-    psd_smooth : (F,) array
-        Smoothed PSD (same shape as input).
-    """
+    def smooth(self, freqs, psd, smooth_factor=None):
+        """
+        Smooth a noisy PSD estimate using a spline in log-log space.
 
-    # remove zero freq if present
-    f = freqs
-    p = psd
+        Parameters
+        ----------
+        freqs : (F,) array
+            Frequency array (must be > 0).
+        psd : (F,) array
+            PSD values (must be > 0).
+        smooth_factor : float or None
+            Smoothing strength. Larger = smoother.
+            If None, an automatic heuristic is used.
 
-    logf = np.log(f)
-    logp = np.log(p)
+        Returns
+        -------
+        psd_smooth : (F,) array
+            Smoothed PSD (same shape as input).
+        """
 
-    if smooth_factor is None:
-        # heuristic: proportional to number of points
-        smooth_factor = len(logf) * 0.2
+        # remove zero freq if present
+        f = freqs
+        p = psd
 
-    # default weights
-    weights = np.ones_like(logp)
+        # Floor before masking
+        # float32 normal range is upto ~1e-38
+        # float32 subnormal range is till ~1e-45
+        # so we pick the smallest representable value > 0
+        p = np.maximum(p, 1e-45)
 
-    # upweight special regions in freq space
-    if upweight_regions is not None:
-        for f_low, f_high in upweight_regions:
-            line_mask = (f >= f_low) & (f <= f_high)
-            weights[line_mask] = 2
+        # define cutoff
+        mask = f >= self.noise_low_frequency_cutoff
 
-    spline = UnivariateSpline(logf, logp, s=smooth_factor, w=weights)
+        # only use trusted region for fitting
+        f = f[mask]
+        p = p[mask]
 
-    logp_smooth = spline(logf)
-    psd_smooth = np.exp(logp_smooth)
+        logf = np.log(f)
+        logp = np.log(p)
 
-    # put back DC if needed
-    out = psd.copy()
-    out = psd_smooth
+        if smooth_factor is None and self.smooth_factor is None:
+            # heuristic: proportional to number of points
+            self.smooth_factor = len(logf) * 0.2
+        elif smooth_factor:
+            self.smooth_factor = smooth_factor
 
-    return out
+        # default weights
+        weights = np.ones_like(logp)
+
+        # upweight special regions in freq space
+        if self.upweight_regions is not None:
+            for f_low, f_high in self.upweight_regions:
+                line_mask = (f >= f_low) & (f <= f_high)
+                weights[line_mask] = 2
+
+        # Number and placement of knots might different between PSDs
+        # Use LSQUnivariateSpline if you want to keep these fixed
+        spline = UnivariateSpline(logf, logp, s=self.smooth_factor, w=weights)
+
+        logp_smooth = spline(logf)
+        psd_smooth = np.exp(logp_smooth)
+
+        # put back DC if needed
+        out = psd.copy()
+        out[mask] = psd_smooth
+
+        return out
