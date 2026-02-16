4e6a3a072ebaa4cca2ab7ea94576784d2c0b8d8f
nnarenraju <nnarenraju@gmail.com>
2026-02-16 00:49:17 +0000

    Added scaling factor to get unit variance

--- sage/dsp/whiten.py
+++ sage/dsp/whiten.py
@@ -44,6 +44,7 @@ class FiducialWhitening(torch.nn.Module):
         fiducial_psds: torch.Tensor,  # (D, F) float32
         seq_len: int,
         sample_rate: float,
+        corrupted_seconds: float = 2,
         device="cuda",
     ):
         super().__init__()
@@ -52,24 +53,33 @@ class FiducialWhitening(torch.nn.Module):
 
         self.seq_len = seq_len
         self.sample_rate = sample_rate
+        self.corrupted_len = int(round(corrupted_seconds * self.sample_rate))
 
         # Frequency resolution
         delta_f = sample_rate / seq_len
         delta_f = torch.tensor(delta_f).to(device=device)
 
-        # Whitening factor:
-        # sqrt(2 * delta_f) / sqrt(PSD)
-        # (factor 2 because one-sided PSD)
-        inv_sqrt_psd = 1.0 / torch.sqrt(fiducial_psds)
-        whitening = torch.sqrt(2.0 * delta_f) * inv_sqrt_psd  # (D, F)
+        # Whitening
+        # Factors account for one sided PSD
+        # We should get unit variance with some sqrt(2) factor
+        inv_sqrt_psd = 1.0 / torch.sqrt(2.0 * fiducial_psds)
+        whitening = torch.sqrt(delta_f * self.seq_len) * inv_sqrt_psd  # (D, F)
+        # Rescaling at DC and Nyquist
+        sqrt2 = torch.sqrt(torch.tensor(2.0, device=device))
+        whitening[..., 0] *= sqrt2
+        whitening[..., -1] *= sqrt2
+        # Final whitening moved to device
         whitening = whitening.to(device=device)
 
         # Register as buffer for compile friendliness
         self.register_buffer("whitening", whitening)  # (D, F)
 
-    @staticmethod
-    def remove_corrupted(self, timeseries):
-        return timeseries[self.pad_len : timeseries.size()[-1] - self.pad_len]
+    def remove_corrupted(self, x):
+        # x_td_white or x: (B, D, T)
+        T = x.shape[-1]
+        start = self.corrupted_len
+        end = T - self.corrupted_len
+        return x[..., start:end]
 
     def whiten(self, X_fd: torch.Tensor) -> torch.Tensor:
         """
@@ -78,11 +88,6 @@ class FiducialWhitening(torch.nn.Module):
 
         # Apply whitening in FD
         X_white = X_fd * self.whitening.unsqueeze(0)
-
-        print(self.whitening)
-
-        plt.plot(self.whitening[0].detach().cpu().numpy())
-        plt.show()
 
         # Back to time domain
         x_td_white = torch.fft.irfft(
@@ -94,9 +99,6 @@ class FiducialWhitening(torch.nn.Module):
         # Remove corrupted regions
         # Typically we remove half the window length used
         # for estimating the PSD in Welch method
-        FiducialWhitening.remove_corrupted(x_td_white)
-
-        plt.plot(x_td_white[0][0].detach().cpu().numpy())
-        plt.show()
+        x_td_white = self.remove_corrupted(x_td_white)
 
         return x_td_white
