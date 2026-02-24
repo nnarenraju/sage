003bea5cb0c1b3a484225317d367cde83d1fd4c3
nnarenraju <nnarenraju@gmail.com>
2026-02-24 16:04:28 +0000

    Minor untracked changes

---
+++ sage/core/base_classes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : base_classes.py
+Description     : Short description of the file
+
+Created on 2026-02-24 16:15:51
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
