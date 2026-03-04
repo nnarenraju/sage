5a123258e6cf9515a5d5f9ec893c377f901f4114
nnarenraju <nnarenraju@gmail.com>
2026-03-04 01:40:29 +0000

    Bringing get_fiducial_psds to surface

---
+++ sage/data/psd/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : __init__.py
+Description     : Short description of the file
+
+Created on 2026-03-02 22:53:53
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
+
+from .read_psds import get_fiducial_psds
