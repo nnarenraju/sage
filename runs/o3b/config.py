f87124e64a654c97700abfe29dcfcdb5f8e6858d
nnarenraju <nnarenraju@gmail.com>
2026-03-16 11:14:27 +0000

    Configuration setting for run

---
+++ runs/o3b/config.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Filename        : config.py
+Description     : Short description of the file
+
+Created on 2026-03-16 11:45:28
+
+__author__        = Narenraju Nagarajan
+__copyright__     = Copyright 2026, ProjectName
+__license__       = MIT Licence
+__version__       = 0.0.1
+__maintainer__    = Narenraju Nagarajan
+__affiliation__   = N/A
+__email__         = N/A
+__status__        = ['inProgress', 'Archived', 'inUsage', 'Debugging']
+
+
+GitHub Repository: NULL
+
+Documentation: NULL
+
+"""
+
+# Packages
+import torch
+
+
+class O3aCFG:
+
+    export_dir = "./export_dir"
+    batch_size = 1024
+    device = "cuda:0"
+    dtype = torch.float32
+    detectors = ["H1", "L1"]
+    do_point_estimate = ["tc", "mchirp"]
+    autocast = True
+    class_balance = 0.5
+    clip_norm = 1.0
+    num_epochs = 400
+    num_iterations = 200_000
+
+
+class O3aDataCFG:
+
+    data_dir = "./data_dir"
+    training_noise_files = [
+        "./data_release/data_H1_O3a.bin",
+        "./data_release/data_L1_O3a.bin",
+    ]
+    validation_noise_files = [
+        "./data_release/data_H1_O3a.bin",
+        "./data_release/data_L1_O3a.bin",
+    ]
+    sample_rate = 2048.0  # Hz
+    noise_low_frequency_cutoff = 15.0  # Hz
+    signal_low_frequency_cutoff = 20.0  # Hz
+    sample_length_in_s = 12.0  # seconds
+    padding_length_in_s = 2.0  # seconds
