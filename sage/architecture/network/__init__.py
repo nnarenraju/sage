3df4f42af8861e48e51255deaa8dde977347b983
nnarenraju <nnarenraju@gmail.com>
2026-03-20 10:28:10 +0000

    Update __init__

--- sage/architecture/network/__init__.py
+++ sage/architecture/network/__init__.py
@@ -23,7 +23,12 @@ Documentation: NULL
 
 """
 
-from .mscnn1d_resnet2d_cbam import MSCNN1D_2DResNetCBAM
+from .mscnn1d_att_resnet2d_cbam import MSCNN1D_2DResNetCBAM
 from .mscnn1d_att_resnet3d_cbam import MSCNN1Datt_3DResNetCBAM
+from .mscnn1d_catt_resnet2d_cbam import MSCNN1D_catt_2DResNetCBAM
 
-__all__ = ["MSCNN1D_2DResNetCBAM", "MSCNN1Datt_3DResNetCBAM"]
+__all__ = [
+    "MSCNN1D_2DResNetCBAM",
+    "MSCNN1Datt_3DResNetCBAM",
+    "MSCNN1D_catt_2DResNetCBAM",
+]
