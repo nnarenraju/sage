8eff28678585ecfd82d20559d120aa3c0356abb7
nnarenraju <nnarenraju@gmail.com>
2026-03-22 13:21:44 +0000

    Updated start script

--- runs/o3b/start.sh
+++ runs/o3b/start.sh
@@ -3,9 +3,6 @@
 # Optional: activate virtual environment if you have one
 # source /path/to/venv/bin/activate
 
-# Register the Sage run configs
-python3 -c "from config import set_configs; set_configs()"
-
 # Make the dataset required for training
 python3 -c "from dataset import make_dataset; make_dataset()"
 
