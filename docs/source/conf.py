b482d3d533d1dcbb5f18a385bbe301bc15e0881b
nnarenraju <nnarenraju@gmail.com>
2026-03-23 15:04:45 +0000

    Updated readthedocs files

---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "sage"
+copyright = "2026, Narenraju Nagarajan"
+author = "Narenraju Nagarajan"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+    "autoapi.extension",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+autoapi_type = "python"
+autoapi_dirs = ["../../sage"]
+autoapi_keep_files = True
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. sage documentation master file, created by
+   sphinx-quickstart on Mon Mar 23 14:25:10 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+sage documentation
+==================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
