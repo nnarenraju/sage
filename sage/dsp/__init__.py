e41b9157ba444021d0126c3e2dd5c3ca61461bed
nnarenraju <nnarenraju@gmail.com>
2026-05-23 00:23:13 +0200

    Added heterodyning

--- sage/dsp/__init__.py
+++ sage/dsp/__init__.py
@@ -5,6 +5,12 @@ from sage.dsp.multibanding import (
     describe_layout,
     make_dyadic_frequency_bands,
 )
+from sage.dsp.heterodyning import (
+    apply_heterodyne,
+    compute_reference_phase,
+    make_median_reference_binary,
+    residual_chirp_time,
+)
 
 __all__ = [
     "FrequencyBand",
@@ -12,4 +18,8 @@ __all__ = [
     "FrequencyMultibandCompressor",
     "describe_layout",
     "make_dyadic_frequency_bands",
+    "apply_heterodyne",
+    "compute_reference_phase",
+    "make_median_reference_binary",
+    "residual_chirp_time",
 ]
