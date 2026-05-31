587336c898a84937c8d854f91a60ec978bff3376
nnarenraju <nnarenraju@gmail.com>
2026-05-31 12:33:01 +0200

    Update imports

--- sage/factory/__init__.py
+++ sage/factory/__init__.py
@@ -1,9 +1,17 @@
 from .training import SageVanillaTraining
 from .curriculum import SageCurriculumTraining
 from .validation import SageVanillaValidation
+from .miner_schedules import (
+    LinearThresholdSchedule,
+    CosineThresholdSchedule,
+    StepThresholdSchedule,
+)
 
 __all__ = [
     "SageVanillaTraining",
     "SageCurriculumTraining",
     "SageVanillaValidation",
+    "LinearThresholdSchedule",
+    "CosineThresholdSchedule",
+    "StepThresholdSchedule",
 ]
