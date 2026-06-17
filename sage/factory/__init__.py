495dbec704483d835a508c95935f3706861bb925
nnarenraju <nnarenraju@gmail.com>
2026-06-17 18:07:18 +0100

    Drop threshold-schedule exports from factory

--- sage/factory/__init__.py
+++ sage/factory/__init__.py
@@ -2,18 +2,10 @@ from .training import SageVanillaTraining
 from .hard_mining import SageHardMiningTraining
 from .consistency_training import SageConsistencyTraining
 from .validation import SageVanillaValidation
-from .miner_schedules import (
-    LinearThresholdSchedule,
-    CosineThresholdSchedule,
-    StepThresholdSchedule,
-)
 
 __all__ = [
     "SageVanillaTraining",
     "SageHardMiningTraining",
     "SageConsistencyTraining",
     "SageVanillaValidation",
-    "LinearThresholdSchedule",
-    "CosineThresholdSchedule",
-    "StepThresholdSchedule",
 ]
