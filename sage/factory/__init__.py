e68ccf34cc1033d2f16e443f6a820aab0d93d74d
nnarenraju <nnarenraju@gmail.com>
2026-06-17 21:29:14 +0100

    Swap SageHardMiningTraining export for HardMiningCallback

--- sage/factory/__init__.py
+++ sage/factory/__init__.py
@@ -1,13 +1,12 @@
 from .training import SageVanillaTraining
-from .hard_mining import SageHardMiningTraining
 from .consistency_training import SageConsistencyTraining
 from .validation import SageVanillaValidation
-from .callbacks import Callback
+from .callbacks import Callback, HardMiningCallback
 
 __all__ = [
     "SageVanillaTraining",
-    "SageHardMiningTraining",
     "SageConsistencyTraining",
     "SageVanillaValidation",
     "Callback",
+    "HardMiningCallback",
 ]
